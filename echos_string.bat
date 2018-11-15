d639461678b823c29d184b2b006c4ac7e4f87d87
505e06b2 <505e06b2@gmail.com>
2018-11-15 16:54:07 +0000

    Fixed usability

--- echos_string.bat
+++ echos_string.bat
@@ -1,18 +1,21 @@
-rem ();int puts(const char *);int main(){/*
+rem ();/*
 @echo off
 echo ^>^>^> Batch:
 goto :main*/
-	#define echo puts(
-	#define rem );
-	#define gg "gg"
-/*
+#include <stdio.h>
+
+#define set char *
+#define echo puts(
+#define rem );
+
+#define text "text"
+int main() {/*
 :main*/
-
-echo gg
-
-rem /*
+	echo text
+	rem /*
 echo ^>^>^> C:
 tcc\tcc -x c "%~0" -run
 pause
 goto :eof
-*/return 0;}
+*/	return 0;
+}
